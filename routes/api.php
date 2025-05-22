cfe909bc72926d92abcd0b08c3bd190d62e76d2e
rahmakhaled1 <rahmakhaled126@gmail.com>
2025-05-22 10:58:35 +0300

    Base v1

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/verify-otp', 'verifyOtp');
+    Route::post('/login', 'login');
+    Route::post('/send-otp', 'sendOtp');
+    Route::post('/reset-password', 'resetPassword');
+     Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
+
